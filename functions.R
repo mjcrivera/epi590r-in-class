4eb2c692f99f87042b3b61dfe30ea3588fc759e6
mjcrivera <mjcrivera@yahoo.com>
2025-08-19 14:43:25 -0400

    2.4 Fuctions Practice

---
+++ functions.R
@@ -0,0 +1,25 @@
+x <- c(1, 3, 5, 7, 9)
+new_mean(x = x)
+new_mean <- function(x) {
+	n <- length(x)
+	mean_val <- sum(x) / n
+	return(mean_val)
+}
+new_mean(x = c(100, 200, 300))
+new_mean <- function(x) {
+	n <- length(x)
+	mean_value <- sum(x) / n
+	return (mean_value)
+}
+y <- c(34563, 234, 7457, 865, 2534)
+
+new_mean (y)
+
+new_mean(x = c(1, 2, 3))
+new_mean <- function(x) {
+	n <- length(x)
+	mean_value <- sum(x) / n
+}
+
+z <- c(80, 40, 30, 25)
+new_mean (z)
